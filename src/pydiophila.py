e6110b4c702c47b7db42323443b34ff35aff998c
Donatus Herre <donatus.herre@slub-dresden.de>
2022-06-14 23:54:14 +0200

    Update diophila script

--- src/pydiophila.py
+++ src/pydiophila.py
@@ -1,3 +1,14 @@
 from diophila import OpenAlex
 
 openalex = OpenAlex("bibliometrie@slub-dresden.de")
+
+api_url = "https://api.openalex.org/works?filter=raw_affiliation_string.search:dresden,publication_year:%3E2016"
+call = openalex.get_works_by_api_url(api_url)
+first_batch = next(call)
+
+filters = {
+    "raw_affiliation_string.search": "dresden",
+    "publication_year": ">2016"
+}
+call = openalex.get_list_of_works(filters=filters, per_page=100)
+first_batch = next(call)
